1e5c55e145b01322b771207cb98eec0b45485580
Alex <abaenadaw@gmail.com>
2017-02-15 23:12:07 +0100

    · Ítems añadidos al menú. · Barra lateral desplegable activada

--- js/javascript.js
+++ js/javascript.js
@@ -3,6 +3,8 @@ $( document ).ready( inicio );
 function inicio() {
 	$('.modal').modal();
 	$('select').material_select();
+	$('.button-collapse').sideNav();
+    //$('.collapsible').collapsible(); Para menís desplegables dentro?
 
 	$('#cbx-otr').on("change", function( ){
 		$('#txt-otr').slideToggle("fast");
