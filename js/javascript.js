6865b73b285b2f146a6fad2077f5fc562cd7c707
ybustos <yeraybl@gmail.com>
2017-02-16 21:29:42 +0100

    - Añadidos estilos de clases para el tamaño de material icons. - Añadida función click para probar toast. - Eliminado modal footer, reemplazándolo por div para poder centrar el botón de 'submit'. - Añadido icono para cerrar la modal.

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,19 @@
 $(document).ready(inicio);
 
 function inicio() {
-	$('.modal').modal();
-	$('select').material_select();
-	$('.button-collapse').sideNav();
+    $('.modal').modal();
+    $('select').material_select();
+    $('.button-collapse').sideNav();
     //$('.collapsible').collapsible(); Para menís desplegables dentro?
 
-	$('#cbx-otr').on("change", function( ){
-		$('#txt-otr').slideToggle("fast");
-		$('#informacion').focus();
+    $('#cbx-otr').on("change", function () {
+        $('#txt-otr').slideToggle("fast");
+        $('#informacion').focus();
 
-	});
+    });
+
+    $('#sendContactMail').click(function () {
+        Materialize.toast('I am a toast!', 3000, 'green');
+    })
 
 }
