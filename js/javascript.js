9f739f0c4119f26b4c1fcbefc11b8257864e5adc
Alex <abaenadaw@gmail.com>
2017-02-16 23:40:22 +0100

    Creada función para recoger valores del modal Creada función que extienda el textarea del modal si "otros" está seleccionado

--- js/javascript.js
+++ js/javascript.js
@@ -13,7 +13,35 @@ function inicio() {
     });
 
     $('#sendContactMail').click(function () {
+        var mail = {};
+        var opciones = [];
+        var datos = ($('.cnt-mail').serializeArray());
+        var cbx = $('.cbx').serializeArray();
+        $.each(datos, function(e, v){
+            mail[v.name] = v.value;
+        });
+        if(cbx.length === 0) {
+            $.each($('#sel-opt').val(), function(e, v){
+                console.log(v);
+                opciones.push(v);
+            });
+        } else {
+            $.each(cbx, function(e, v){
+                opciones.push(v.value);
+            });
+        }
+        mail['opciones'] = opciones;
+
+        console.log(JSON.stringify(mail));
         Materialize.toast('I am a toast!', 3000, 'green');
-    })
+    });
+
+    $('#sel-opt').change(function(){
+        if($.inArray("otr", $(this).val()) != -1){
+            $("#txt-otr").slideDown("fast");
+        }else{
+            $("#txt-otr").hide("fast");
+        }
+    });
 
 }
