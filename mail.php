4f7b1f195b44c4fdc97ec0f38780b41f2eb1248b
Alex <abaenadaw@gmail.com>
2017-02-18 21:07:36 +0100

    Toqueteos en el mail

--- mail.php
+++ mail.php
@@ -2,7 +2,8 @@
 
 $to = 'info@codecube.es';
 
-$headers = "MIME-Version: 1.0\r\n";
+$headers = "From: codecube.es\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=utf-8\r\n";
 
 $nombre = $_POST['name'];
